2e02046f975acd2ae50cf742c98db45c5bd67b14
ItsWitchy <kikilovesfoals@gmail.com>
2019-12-12 15:17:52 -0600

    Add files via upload

---
+++ Check_User_Status.ps1
@@ -0,0 +1,9 @@
+﻿$csv = Import-Csv -Path '.\OneDrive - OPRG\Work\Scripts\STLUsers.csv'
+
+ForEach($name in $csv)
+{
+    Get-ADUser -filter $name -Properties whenChanged, lastLogon
+}
+
+Select-Object SamAccountName, whenChanged, lastLogon
+Export-Csv -Path '.\OneDrive - OPRG\Work\Scripts\STLUsers_Done.csv'
